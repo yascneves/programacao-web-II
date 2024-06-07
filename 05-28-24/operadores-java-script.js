8c159b7d658ba46b3a19988b19074af3590a8790
Yasmim <yascneves@hotmail.com>
2024-06-07 17:04:07 -0300

    aulas de maio/24

---
+++ 05-28-24/operadores-java-script.js
@@ -0,0 +1,9 @@
+// Exercícios de operação em java script
+// considerando as operações abaixo, indique a aplicação realizada e o valor de x a cada iteração
+// a) x = 8
+// b) x+=10
+// c) x-=-1
+// d) x*=4
+// e) xdividido=2
+// f) x%=8
+// g) x**=2
---
+++ 05-28-24/operadores-matematicos.js
@@ -0,0 +1,37 @@
+// x = 8
+// alert(x)
+
+// x+=10
+// alert(x)
+
+// x-=-1
+// alert(x)
+
+// x *= 4
+// alert(x)
+
+// x/=2
+// alert(x)
+
+// x%=8
+// alert(x)
+
+// x**=2
+// alert(x)
+
+// var A = 8
+// var B = 12
+// var C = -3
+
+// alert(2 * (A % 3) - C)
+// alert((B + 6) / 4)
+// alert(((27/3)/3) +2**4)
+// alert(((30 % 4) *3**2) * - 1)
+// alert(A+(((Math.sqrt (A + 1)** 2) / (B + C)) - (A - 1)))
+
+var num1 = prompt("Digite o valor de uma variável:")
+var num1Int = parseInt(num1)
+var num1Float = parseFloat(num1)
+var num2 = 10
+var igual = num1 != num2
+alert(igual)
